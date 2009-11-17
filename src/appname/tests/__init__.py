6dc3eff97805f60df7f11f60c164690a5affc9fd
Gregor Müllegger <gregor@muellegger.de>
2009-11-17 12:21:56 +0100

    adding ability to test django app

---
+++ src/appname/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+class Test(TestCase):
+    def test(self):
+        self.assert_(True)
---
+++ src/appname/tests/settings.py
@@ -0,0 +1,3 @@
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = 'sqlite.db'
+INSTALLED_APPS = ('appname',)
