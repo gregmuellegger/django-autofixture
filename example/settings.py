b4df9dcafbea2d50822d2aa7d9af69ae17d9497c
Gregor Müllegger <gregor@muellegger.de>
2010-02-27 21:48:48 +0100

    Adding sites framework to example project.

--- example/settings.py
+++ example/settings.py
@@ -76,6 +76,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.admin',
 
     'django_autofixture',
