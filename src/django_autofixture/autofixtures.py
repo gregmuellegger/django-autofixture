e1140d5e90a0123bc152b0319bdd44979bba4b90
Gregor Müllegger <gregor@muellegger.de>
2010-03-05 21:19:54 +0100

    Adding UserFixture.

---
+++ src/django_autofixture/autofixtures.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import django_autofixture
+import string
+from datetime import datetime
+from django.contrib.auth.models import User, UNUSABLE_PASSWORD
+from django_autofixture import AutoFixture
+from django_autofixture import generators
+
+
+class UserFixture(AutoFixture):
+    field_values = {
+        'username': generators.StringGenerator(chars=
+            string.ascii_letters + string.digits + '_'),
+        'first_name': generators.LoremWordGenerator(1),
+        'last_name': generators.LoremWordGenerator(1),
+        'password': UNUSABLE_PASSWORD,
+        # generate ten times more activated than deactivated users.
+        'is_active': generators.ChoicesGenerator(values=[True] * 10 + [False]),
+        # don't generate admin users
+        'is_staff': False,
+        'is_superuser': False,
+        'date_joined': generators.DateTimeGenerator(max_date=datetime.now()),
+        'last_login': generators.DateTimeGenerator(max_date=datetime.now()),
+    }
+
+    # don't follow permissions and groups
+    follow_m2m = False
+
+    def unique_email(self, model, instance):
+        if User.objects.filter(email=instance.email):
+            raise django_autofixture.InvalidConstraint(('email',))
+
+    def prepare_class(self):
+        self.add_constraint(self.unique_email)
+
+    def post_process_instance(self, instance):
+        if instance.last_login < instance.date_joined:
+            instance.last_login = instance.date_joined
+        return instance
+
+
+django_autofixture.register(User, UserFixture, fail_silently=True)
