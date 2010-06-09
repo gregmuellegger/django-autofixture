1dcbfab8a7b302263b647f106ebad138da7f123f
Gregor Müllegger <gregor@muellegger.de>
2010-06-09 11:24:43 +0200

    Fixing long_description compilation in setup file.

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ class UltraMagicString(object):
         return self.value.split(*args, **kw)
 
 
-long_description = UltraMagicString(u'\n\n'.join(
+long_description = UltraMagicString(u'\n\n'.join((
     file('README').read(),
     file('CHANGES').read(),
-))
+)))
 
 
 # determine package version
