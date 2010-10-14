07226a51b1e369caf8dcdbd2bfa64a5d05252561
Gregor Müllegger <gregor@muellegger.de>
2010-10-15 00:52:35 +0200

    Bumping version number.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ long_description = UltraMagicString(u'\n\n'.join((
 
 setup(
     name = 'django-autofixture',
-    version = '0.2.5',
+    version = '0.3.0pre',
     url = 'https://github.com/gregmuellegger/django-autofixture',
     license = 'BSD',
     description = 'Provides tools to auto generate test data.',
