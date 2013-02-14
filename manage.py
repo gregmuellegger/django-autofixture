c9b7896d0c2ec15328437b584982501dc81223b4
Gregor Müllegger <gregor@muellegger.de>
2013-02-14 23:20:54 +0100

    Adding a manage.py script.

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import os
+import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autofixture_tests.settings")
 
-
-if __name__ == '__main__':
     from django.core.management import execute_from_command_line
-    execute_from_command_line()
+
+    execute_from_command_line(sys.argv)
