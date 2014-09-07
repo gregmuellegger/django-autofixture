2127360b995b695e08fd7328a513b8622348e156
ad-m <ad_m@dobrytata.pl>
2014-09-08 01:31:07 +0200

    Add support to model with strings in max_lenght
    Django code:
    ````
      title = models.CharField(max_length=250)
    ````
    and 
    ````
      title = models.CharField(max_length="250")
    ````
    are equal, but one of them make django-autofixture fail. This fix that.

--- autofixture/generators.py
+++ autofixture/generators.py
@@ -158,7 +158,7 @@ class LoremGenerator(Generator):
                 paras = ['<p>%s</p>' % p for p in paras]
             lorem = u'\n\n'.join(paras)
         if self.max_length:
-            length = random.randint(round(self.max_length / 10),
+            length = random.randint(round(int(self.max_length) / 10),
                                     self.max_length)
             lorem = lorem[:max(1, length)]
         return lorem.strip()
