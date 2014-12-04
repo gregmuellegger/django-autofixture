3f9f13a1a1d96d997f012b331f0af7d7d0b085d6
Gregor Müllegger <gregor@muellegger.de>
2014-12-04 12:47:47 +0100

    Giving the database a name in the test settings to allow a local sqlite db for manual testing.

--- autofixture_tests/settings.py
+++ autofixture_tests/settings.py
@@ -8,6 +8,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
     'default': {
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite'),
         'ENGINE': 'django.db.backends.sqlite3',
     }
 }
