e523531e4b67f7d78220f6b0502e1c135f14d016
Gregor Müllegger <gregor@muellegger.de>
2015-04-09 14:54:58 +0200

    Removing unnecessary ADMIN_MEDIA_PREFIX setting from test settings.

--- autofixture_tests/settings.py
+++ autofixture_tests/settings.py
@@ -33,11 +33,6 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
-
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '0'
 
