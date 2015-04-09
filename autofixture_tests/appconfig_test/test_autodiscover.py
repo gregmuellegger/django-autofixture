454838346488e90af8f6ca2ad81dff88f686c8d4
Gregor Müllegger <gregor@muellegger.de>
2015-04-09 14:57:41 +0200

    Skipping AppConfig tests on Django<1.7

--- autofixture_tests/appconfig_test/test_autodiscover.py
+++ autofixture_tests/appconfig_test/test_autodiscover.py
@@ -1,11 +1,14 @@
+import django
 from django.conf import settings
 from django.test import TestCase
 
 import autofixture
+from ..compat import skipIf
 from .models import TestModel
 
 
 class AutodiscoverTest(TestCase):
+    @skipIf(django.VERSION < (1, 7), 'AppConfig only applies to Django >= 1.7')
     def test_testmodel_fixture_was_loaded(self):
         self.assertTrue(any(
             app for app in settings.INSTALLED_APPS
---
+++ autofixture_tests/compat.py
@@ -0,0 +1,4 @@
+try:
+    from unittest import skipIf
+except ImportError:
+    from django.utils.unittest import skipIf
